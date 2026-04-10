e204abad4c95464360a16e59b0656e37f9a4aef6
Kenechukwu Agbo <kennygeemartin@gmail.com>
2026-04-10 14:07:01 +0100

    Add BE-RPL header file

--- be_rpl.h
+++ be_rpl.h
@@ -1,12 +1,28 @@
 #ifndef BE_RPL_H
 #define BE_RPL_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define MAX_NEIGHBORS 20
+#define MAX_PARENTS 5
+
 typedef struct {
-    // Define your struct members here
+    int id;
+    float etx;
+    float energy;
+    float queue;
+    float stability;
+    float score;
 } parent_t;
 
-// Function declarations
-void initialize_parents(parent_t *parents);
-void select_parent(parent_t *parents);
+extern parent_t neighbors[MAX_NEIGHBORS];
+extern int neighbor_count;
 
-#endif // BE_RPL_H
+float compute_score(parent_t p);
+void select_parents();
+parent_t select_best_parent();
+void fast_local_repair();
+
+#endif

e8e1e74b8147c323c23783d9bb35f2ade5746c51
Kenechukwu Agbo <kennygeemartin@gmail.com>
2026-04-10 14:04:37 +0100

    Create be_rpl.h with parent_t struct and function declarations

---
+++ be_rpl.h
@@ -0,0 +1,12 @@
+#ifndef BE_RPL_H
+#define BE_RPL_H
+
+typedef struct {
+    // Define your struct members here
+} parent_t;
+
+// Function declarations
+void initialize_parents(parent_t *parents);
+void select_parent(parent_t *parents);
+
+#endif // BE_RPL_H
